996ef37165a3d1377071f6ce398b113e0ee60658
David Öhman <dohman_92@hotmail.com>
2018-09-09 17:10:15 +0200

    Added start menu and highscore

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour {
 
     public Text countText;
-
+    public TextMeshProUGUI highscoreText;
     public static int score;
+    public static int highscore;
 
     private void Start()
     {
-        SetText();
+        if (countText != null)
+            SetText();
+
+        highscore = PlayerPrefs.GetInt("highscore", highscore);
+
+        if (highscoreText != null)
+            highscoreText.text = "Highscore: " + highscore.ToString();
+    }
+
+    private void Update()
+    {
+        if (score > highscore)
+        {
+            highscore = score;
+            //highscoreText.text = "" + score.ToString();
+
+            PlayerPrefs.SetInt("highscore", highscore);
+        }
     }
 
     public int IncreaseScore()
